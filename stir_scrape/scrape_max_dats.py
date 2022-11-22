dd7466850003c2e657da1a2c66d87d4d78f412c5
Zac Johnston <zacpjohnston@gmail.com>
2022-11-22 17:05:15 -0500

    [stir_scrape/] adapted scrape_tools.py

--- stir_scrape/scrape_max_dats.py
+++ stir_scrape/scrape_max_dats.py
@@ -14,15 +14,19 @@ if __name__ == "__main__":
                 'mdot',
                 'turb_antesonic',
                 ]
+    alphas = ['1.25']
 
-    table = pd.DataFrame({'mass': masses})
+    for alpha in alphas:
+        table = pd.DataFrame({'mass': masses})
 
-    var_lists = scrape_tools.get_max_dats(dat_vars=dat_vars, masses=masses)
+        var_lists = scrape_tools.get_max_dats(dat_vars=dat_vars,
+                                              masses=masses,
+                                              alpha=alpha)
 
-    data = pd.DataFrame(var_lists)
-    table = pd.concat([table, data], axis=1)
+        data = pd.DataFrame(var_lists)
+        table = pd.concat([table, data], axis=1)
 
-    table.to_csv(f'out/max_dats.csv', index=False)
+        table.to_csv(f'out/max_dats.csv', index=False)
 
     t1 = time.time()
     print(f'time: {t1-t0:.3f} s')
---
+++ stir_scrape/scrape_tools.py
@@ -0,0 +1,142 @@
+import pandas as pd
+import numpy as np
+import math
+
+from flashbang.simulation import Simulation
+
+
+# =======================================================
+#                    loading models
+# =======================================================
+def load_sim(mass,
+             alpha,
+             load_all=True):
+    """Load model
+
+    Returns : Simulation
+
+    parameters
+    ----------
+    mass : str
+    alpha : str
+    load_all : bool
+    """
+    run = f'stir2_14may19_s{mass}_alpha{alpha}'
+    model = f'run_{mass}'
+    model_set = f'run_14may19_a{alpha}'
+
+    sim = Simulation(run=run,
+                     model=model,
+                     model_set=model_set,
+                     load_all=load_all,
+                     config='stir')
+
+    return sim
+
+
+# =======================================================
+#                   max values
+# =======================================================
+def get_max_dats(dat_vars, masses, alpha):
+    """Return list of max values of .dat variables
+
+    Returns : {var: []}
+
+    parameters
+    ----------
+    dat_vars : [str]
+    masses : [str]
+    alpha :str
+    """
+    lists = {}
+    for var in dat_vars:
+        lists[var] = []
+
+    for mass in masses:
+        model = load_sim(mass=mass, alpha=alpha, load_all=False)
+        model.load_dat()
+
+        for var in dat_vars:
+            lists[var] += [model.dat[var].max()]
+
+    return lists
+
+
+def get_max_times(dat_vars, masses, alpha):
+    """Return list of times of max values of .dat variables
+
+    Returns : {var: []}
+
+    parameters
+    ----------
+    dat_vars : [str]
+    masses : [str]
+    alpha :str
+    """
+    lists = {}
+    for var in dat_vars:
+        lists[var] = []
+
+    for mass in masses:
+        model = load_sim(mass=mass, alpha=alpha, load_all=False)
+        model.load_dat()
+
+        for var in dat_vars:
+            idx = model.dat[var].idxmax()
+            lists[var] += [model.dat.loc[idx, 'time']]
+
+    return lists
+
+
+# =======================================================
+#                      dat
+# =======================================================
+def get_dat(dat_vars, masses, alpha, dt=5e-5):
+    """Returns reduced dat tables from set of models
+
+    Returns: {mass: pd.DataFrame}
+
+    Parameters
+    ----------
+    dat_vars : [str]
+    masses : [str]
+    alpha : str
+    dt : float
+    """
+    dats = {}
+
+    for mass in masses:
+        model = load_sim(mass=mass, alpha=alpha)
+        model.load_dat()
+
+        dats[mass] = interpolate_dat(dat_table=model.dat,
+                                     dt=dt,
+                                     dat_vars=dat_vars)
+
+    return dats
+
+
+def interpolate_dat(dat_table, dt, dat_vars):
+    """Interpolates dat table onto regular timesteps
+
+    Returns: pd.DataFrame
+
+    parameters
+    ----------
+    dat_table : pd.DataFrame
+    dt : float
+        timestep size to interpolate onto (s)
+    dat_vars : [str]
+    """
+    t_end = dat_table.time.iloc[-1]
+    t_end = math.floor(t_end/dt) * dt  # last timestep before t_end
+    n_steps = int(t_end/dt + 1)
+
+    timesteps = np.linspace(0, t_end, n_steps)
+    table = pd.DataFrame()
+    table['time'] = timesteps
+
+    for var in dat_vars:
+        table[var] = np.interp(timesteps, dat_table['time'], dat_table[var])
+
+    return table

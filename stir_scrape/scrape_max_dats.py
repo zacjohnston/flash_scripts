a38d41cd41eafe7e05edeabf4be8f53aaa126b8c
Zac Johnston <zacpjohnston@gmail.com>
2022-11-22 17:37:41 -0500

    [stir_scrape/] add alpha to filename

--- stir_scrape/scrape_max_dats.py
+++ stir_scrape/scrape_max_dats.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
         data = pd.DataFrame(var_lists)
         table = pd.concat([table, data], axis=1)
 
-        table.to_csv(f'out/max_dats.csv', index=False)
+        table.to_csv(f'out/max_dats_alpha{alpha}.csv', index=False)
 
     t1 = time.time()
     print(f'time: {t1-t0:.3f} s')

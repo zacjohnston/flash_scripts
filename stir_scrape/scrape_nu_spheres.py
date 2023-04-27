9ebc00f0a46403103ad7f8a69031e91d7246986f
Zac Johnston <zacpjohnston@gmail.com>
2023-04-27 14:04:50 -0400

    [scrape_nu_spheres.py] created script

---
+++ stir_scrape/scrape_nu_spheres.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import time 
+
+import scrape_tools
+from mass_list import masses
+
+
+if __name__ == "__main__":
+    t0 = time.time()
+    alphas = ['1.25']
+
+    for alpha in alphas:
+        var_lists = scrape_tools.get_nusphere(masses=masses, alpha=alpha)
+
+        table = pd.DataFrame(var_lists)
+        table.to_csv(f'out/nusphere_alpha{alpha}.csv', index=False)
+
+    t1 = time.time()
+    print(f'time: {t1-t0:.3f} s')
+
